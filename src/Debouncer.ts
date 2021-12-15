c84041daafb95e9cbdf6af620371780e096b8ac1
Jakob Westhoff <jakob@westhoffswelt.de>
2021-12-15 23:44:15 +0100

    Implement generic Debouncer
    This Debouncer implementation allows to debounce events, but a specified amount of time. It is created with ease of use in mind. In general it works by simply encapsulating the event handler to debounce wihtin one call to Debouncer.debounce, in order to achieve the desired result.

---
+++ src/Debouncer.ts
@@ -0,0 +1,41 @@
+export type DebouncedCallback<T> = T & { clear: () => void };
+
+export class Debouncer<T extends Function> {
+  public static debounce<T extends Function>(
+    callback: T,
+    delay: number
+  ): DebouncedCallback<T> {
+    const debouncer = new Debouncer<T>(callback, delay);
+    return debouncer.getDebouncedCallback();
+  }
+
+  private debounceTimeout: number;
+  private debouncedCallback: DebouncedCallback<T>;
+
+  constructor(private callback: T, private delay: number) {
+    this.debounceTimeout = undefined;
+    const debouncedCallback = (...args: unknown[]) => {
+      if (this.debounceTimeout !== undefined) {
+        window.clearTimeout(this.debounceTimeout);
+        this.debounceTimeout = undefined;
+        console.log("cleared old debounce");
+      }
+      this.debounceTimeout = window.setTimeout(() => {
+        this.debounceTimeout = undefined;
+        this.callback(...args);
+      }, this.delay);
+      console.log("debounce timeout set");
+    };
+    debouncedCallback.clear = () => {
+      if (this.debounceTimeout !== undefined) {
+        window.clearTimeout(this.debounceTimeout);
+      }
+    };
+
+    this.debouncedCallback = debouncedCallback as any;
+  }
+
+  public getDebouncedCallback(): DebouncedCallback<T> {
+    return this.debouncedCallback;
+  }
+}
