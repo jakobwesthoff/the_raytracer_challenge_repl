709afc9e8897344842b8045bdbbcb2cad94999ba
Jakob Westhoff <jakob@westhoffswelt.de>
2021-12-15 23:45:24 +0100

    Add CodeMirror wrapper
    The `Codemirror` component is a very minimalistic wrapper to utilize a CodeMirrror editor within our Svelte app.

---
+++ src/components/Codemirror.svelte
@@ -0,0 +1,31 @@
+<script context="module" lang="ts">
+  export type EditorEvent = {
+    editor: Editor;
+  };
+</script>
+
+<script lang="ts">
+  import CodeMirror, { Editor, EditorConfiguration } from "codemirror";
+  import "codemirror/mode/yaml/yaml";
+  import "codemirror/lib/codemirror.css";
+
+  import { onMount, createEventDispatcher } from "svelte";
+
+  export let config: EditorConfiguration = {};
+
+  let dispatch = createEventDispatcher();
+
+  let container: HTMLElement;
+
+  let editor: Editor;
+
+  onMount(() => {
+    editor = CodeMirror(container, config);
+    setTimeout(() => dispatch("editor", { editor } as EditorEvent), 100);
+  });
+</script>
+
+<div bind:this={container} />
+
+<style>
+</style>
