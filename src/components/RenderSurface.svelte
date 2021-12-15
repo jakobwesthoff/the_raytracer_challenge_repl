d2e619bf244ec0f2e7e17145167dab8c51852726
Jakob Westhoff <jakob@westhoffswelt.de>
2021-12-15 23:52:37 +0100

    Add RenderSurface to create and display raytraced scenes
    The RenderSurface is the svelte side of the rendering cycle, which is handled by a RenderPool. It is responsible, for rendering a scene using a RenderPool and displaying the render progress, as well as the final result image.

---
+++ src/components/RenderSurface.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import type { AcquiredMutex, Mutex } from "../Mutex";
+  import type { RenderPool } from "../RenderPool";
+
+  export let renderPoolMutex: Mutex<RenderPool>;
+
+  let pool: AcquiredMutex<RenderPool>;
+  let canvasRef: HTMLCanvasElement;
+  let initialized = false;
+  let renderedYaml: string;
+
+  export const render = async (yaml: string) => {
+    renderedYaml = yaml;
+    if (pool !== undefined) {
+      console.time("Stopping old render");
+      await pool.stop();
+      console.timeEnd("Stopping old render");
+    }
+    console.time("Waiting to render");
+    pool = await renderPoolMutex.acquire();
+    console.timeEnd("Waiting to render");
+    console.time("Rendering");
+    await pool.loadYaml(yaml);
+    let cameras = await pool.getCameras();
+    let cameraToRender = cameras[0];
+    canvasRef.setAttribute("width", `${cameraToRender.width}`);
+    canvasRef.setAttribute("height", `${cameraToRender.height}`);
+    let ctx = canvasRef.getContext("2d");
+    await pool.render(cameraToRender.id, (data, { x1, y1 }) => {
+      ctx.putImageData(data, x1, y1);
+    });
+    console.timeEnd("Rendering");
+    pool.release();
+    pool = undefined;
+  };
+
+  onMount(async () => {
+    initialized = true;
+
+    return () => {
+      pool.release();
+    };
+  });
+</script>
+
+<div class="container">
+  <canvas bind:this={canvasRef} />
+</div>
+
+<style>
+  .container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+  }
+
+  canvas {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+</style>
