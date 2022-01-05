359328a6dcac72d53a4f88a3db14ae509d83a90d
Jakob Westhoff <jakob@westhoffswelt.de>
2022-01-05 14:09:07 +0100

    Fixed release script to add new static website files

--- scripts/release-website.sh
+++ scripts/release-website.sh
@@ -2,13 +2,15 @@
 set -euxo pipefail
 npm run clean
 npm run build
+mv -v public release
 VERSION="$(jq -r .version package.json)"
 HASH="$(git rev-parse --short HEAD)"
 git checkout raytracer.xyz
-cp -vR public/* ./
+cp -vR release/* ./
 while read -r path; do
-    git add "${path#./public/}"
-done < <(find ./public -mindepth 1)
+    git add "${path#./release/}"
+done < <(find ./release -mindepth 1)
 git ci -m "Release version ${VERSION} (${HASH})"
 git push origin raytracer.xyz
 git checkout main
+rm -rf release
