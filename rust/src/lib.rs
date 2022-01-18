fce5a8a160752abc7caf89778c5afffc299d86b7
Jakob Westhoff <jakob@westhoffswelt.de>
2022-01-18 20:39:43 +0100

    Comment no longer used console_log macro
    If we need it in the future, we can always google it or retrieve it from
    the git history

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -11,18 +11,6 @@ use js_sys::Error;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-macro_rules! console_log {
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-
 macro_rules! throw_js_error {
     ($($t: tt)*) => (Err(Error::new(&(format_args!($($t)*)).to_string()).into()))
 }
